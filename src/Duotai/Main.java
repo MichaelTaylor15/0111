dd7ea9f672d2b5d1bca80d85caf5b82c181bad81
Administrator <468679810@qq.com>
2021-01-11 10:52:53 +0800

    0

--- src/Duotai/Main.java
+++ src/Duotai/Main.java
@@ -2,7 +2,9 @@ package Duotai;
 
 public class Main {
     public static void main(String[] args) {
-        Animal animal=new Cat();
+        Animal animal=new Cat();//上转型
+        animal.bark();
+        animal=new Dog();
         animal.bark();
     }
 }
